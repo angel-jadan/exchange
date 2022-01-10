1481458ed8615f2741270ee1f41fa13f2f446fdb
Angel <angel_jadan@outlook.com>
2022-01-09 21:44:23 -0500

    config routers

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from "vue"; //path relativo para npm
 import App from "./App.vue";
-import "@/assets/css/tailwind.css"
+import "@/assets/css/tailwind.css";
+
+// rutas
+import router from '@/router';
 
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router: router,
   render: (h) => h(App),
 }).$mount("#app"); //mount proposito el que renderiza en el template
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <PxAssetsTable/> 
+    </div>
+</template>
+
+
+<script>
+import PxAssetsTable from '@/components/PxAssetsTable.vue';
+
+export default {
+    name: 'Home',
+
+    components: {
+        PxAssetsTable,    
+    }
+}
+</script>
